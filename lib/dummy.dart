d8c80d1e5e2587577e5b01ab1223f6cd55ef86f1
saiful <saiful15-13285@diu.edu.bd>
2022-06-20 00:03:53 +0600

    all done

--- lib/dummy.dart
+++ lib/dummy.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/dummy_detail.dart';
 
 class Dummy extends StatelessWidget {
-  const Dummy({Key? key, this.title, this.imageUrl,this.description, this.onTap}) : super(key: key);
+  const Dummy(
+      {Key? key, this.title, this.imageUrl, this.description, this.onTap})
+      : super(key: key);
   final String? title;
   final String? imageUrl;
   final String? description;
@@ -17,21 +18,23 @@ class Dummy extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-            color: Colors.deepPurple,
-            child: Image.network('$imageUrl'),height: 70,
-            
+              color: Colors.deepPurple,
+              // ignore: sort_child_properties_last
+              child: Image.network('$imageUrl'),
+              height: 70,
             ),
           ),
-          Expanded
-          (child: Column(
-         crossAxisAlignment: CrossAxisAlignment.start,
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              
-              Text(title!,style:TextStyle(fontWeight: FontWeight.bold),),
+              Text(
+                title!,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
               Text(description!),
             ],
           )),
-           
         ],
       ),
     );
